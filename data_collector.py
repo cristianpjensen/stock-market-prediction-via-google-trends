0c0777194645f9c07458f80508fa4dc69d26461b
Cristian Perez Jensen <cristianpjensen@gmail.com>
2020-06-04 21:56:19 +0200

    Now reads it into memory, instead of downloading

--- data_collector.py
+++ data_collector.py
@@ -1,189 +1,122 @@
 import datetime
 import json
 import sys
-import os
-import urllib.request
 
 import requests
 from dateutil.relativedelta import relativedelta
+import pandas as pd
 
 
-HEADER = "date,relative_frequency\n"
+class Trends():
+    def __init__(self):
+        if len(sys.argv) != 2:
+            sys.exit("Usage: python3 data_collector.py keyword")
 
+        self.start_date = datetime.date(2004, 1, 1)
+        self.end_date = datetime.date.today()
+        self.keyword = sys.argv[1]
 
-def main():
-    if len(sys.argv) != 4:
-        sys.exit("Usage: python3 data_collector.py keyword start_date end_date")
+        self.get_daily()
+        print(self.daily)
+        self.get_weekly()
+        print(self.weekly)
+        self.get_monthly()
+        print(self.monthly)
 
-    # Turn the dates into datetime types.
-    try:
-        start_date = datetime.date.fromisoformat(sys.argv[2])
-    except:
-        if sys.argv[2].lower() == "start":
-            start_date = datetime.date(year=2004, month=1, day=1)
-        else:
-            sys.exit("Invalid start_date format.")
+    def get_daily(self):
+        print("Downloading daily data...")
 
-    try:
-        end_date = datetime.date.fromisoformat(sys.argv[3])
-    except:
-        if sys.argv[3].lower() == "today":
-            end_date = datetime.date.today()
-        else:
-            sys.exit("Invalid end_date format.")
+        start_increment = self.start_date
+        end_increment = self.start_date + relativedelta(months=+6)
+        dataframe = False
 
-    # Check if the dates given are within Google Trends timerange.
-    if start_date < datetime.date(year=2004, month=1, day=1):
-        sys.exit("Google Trends data wasn't collected pre-2004.")
-    elif end_date > datetime.date.today():
-        sys.exit("Future Google Trends data isn't collected yet.")
+        while True:
+            token = self.get_token(
+                f"{start_increment} {end_increment}")
 
-    keyword = sys.argv[1]
+            url = f"https://trends.google.com/trends/api/widgetdata/multiline/csv?req=%7B%22time%22%3A%22{str(start_increment)}%20{str(end_increment)}%22%2C%22resolution%22%3A%22DAY%22%2C%22locale%22%3A%22en-US%22%2C%22comparisonItem%22%3A%5B%7B%22geo%22%3A%7B%22country%22%3A%22US%22%7D%2C%22complexKeywordsRestriction%22%3A%7B%22keyword%22%3A%5B%7B%22type%22%3A%22BROAD%22%2C%22value%22%3A%22{self.keyword}%22%7D%5D%7D%7D%5D%2C%22requestOptions%22%3A%7B%22property%22%3A%22%22%2C%22backend%22%3A%22IZG%22%2C%22category%22%3A0%7D%7D&token={token}&tz=-120"
 
-    # Create necessary folders.
-    try:
-        os.mkdir("data")
-    except:
-        pass
+            if not dataframe:
+                self.daily = pd.read_csv(url, header=1)
+                dataframe = True
+            else:
+                temp = pd.read_csv(url, header=1)
+                self.daily = self.daily.append(temp, ignore_index=True)
 
-    try:
-        os.mkdir(f"data/{keyword}")
-    except:
-        pass
+            # Daily data can be downloaded in 6-month increments.
+            start_increment += relativedelta(months=+6)
+            end_increment += relativedelta(months=+6)
 
-    try:
-        os.mkdir(f"data/{keyword}/{str(start_date)}_{end_date}")
-        os.mkdir(f"data/{keyword}/{str(start_date)}_{end_date}/unadjusted")
-        os.mkdir(
-            f"data/{keyword}/{str(start_date)}_{end_date}/unadjusted/daily")
-        os.mkdir(
-            f"data/{keyword}/{str(start_date)}_{end_date}/unadjusted/weekly")
-    except:
-        sys.exit("You have already collected data for that keyword in that timespan.")
+            if start_increment > self.end_date:
+                self.daily = self.daily.rename(
+                    columns={"Day": "date", "debt: (United States)": "relative_frequency"})
+                return
+            else:
+                # Remove overlap.
+                self.daily = self.daily[:-1]
 
-    # Pull data.
-    get_monthly(keyword, start_date, end_date)
-    get_weekly(keyword, start_date, end_date)
-    get_daily(keyword, start_date, end_date)
+    def get_weekly(self):
+        print("Downloading weekly data...")
 
+        start_increment = self.start_date
+        end_increment = self.start_date + relativedelta(years=+5)
+        dataframe = False
 
-def get_daily(keyword, start_date, end_date):
-    print("Downloading daily data...")
+        while True:
+            token = self.get_token(f"{start_increment} {end_increment}")
 
-    start_increment = start_date
-    end_increment = start_date + relativedelta(months=+6)
-    index = 0
+            url = f"https://trends.google.com/trends/api/widgetdata/multiline/csv?req=%7B%22time%22%3A%22{str(start_increment)}%20{str(end_increment)}%22%2C%22resolution%22%3A%22WEEK%22%2C%22locale%22%3A%22en-US%22%2C%22comparisonItem%22%3A%5B%7B%22geo%22%3A%7B%22country%22%3A%22US%22%7D%2C%22complexKeywordsRestriction%22%3A%7B%22keyword%22%3A%5B%7B%22type%22%3A%22BROAD%22%2C%22value%22%3A%22{self.keyword}%22%7D%5D%7D%7D%5D%2C%22requestOptions%22%3A%7B%22property%22%3A%22%22%2C%22backend%22%3A%22IZG%22%2C%22category%22%3A0%7D%7D&token={token}&tz=-120"
 
-    while True:
-        token = get_token(keyword, f"{start_increment} {end_increment}")
+            if not dataframe:
+                self.weekly = pd.read_csv(url, header=1)
+                dataframe = True
+            else:
+                temp = pd.read_csv(url, header=1)
+                self.weekly = self.weekly.append(temp, ignore_index=True)
 
-        url = f"https://trends.google.com/trends/api/widgetdata/multiline/csv?req=%7B%22time%22%3A%22{str(start_increment)}%20{str(end_increment)}%22%2C%22resolution%22%3A%22DAY%22%2C%22locale%22%3A%22en-US%22%2C%22comparisonItem%22%3A%5B%7B%22geo%22%3A%7B%22country%22%3A%22US%22%7D%2C%22complexKeywordsRestriction%22%3A%7B%22keyword%22%3A%5B%7B%22type%22%3A%22BROAD%22%2C%22value%22%3A%22{keyword}%22%7D%5D%7D%7D%5D%2C%22requestOptions%22%3A%7B%22property%22%3A%22%22%2C%22backend%22%3A%22IZG%22%2C%22category%22%3A0%7D%7D&token={token}&tz=-120"
-        urllib.request.urlretrieve(
-            url, f"data/{keyword}/{str(start_date)}_{str(end_date)}/unadjusted/daily/temp.csv")
+            # Weekly data can be downloaded in 5-year increments.
+            start_increment += relativedelta(years=+5)
+            end_increment += relativedelta(years=+5)
 
-        # Remove first two lines.
-        with open(f"data/{keyword}/{str(start_date)}_{str(end_date)}/unadjusted/daily/temp.csv", "r") as f:
-            with open(f"data/{keyword}/{str(start_date)}_{str(end_date)}/unadjusted/daily/{index}_daily_{keyword}.csv", "w") as f1:
-                for _ in range(3):
-                    next(f)
-                f1.write(HEADER)
-                lines = f.readlines()
-                if end_increment < end_date:
-                    lines.pop()
-                f1.writelines(lines)
+            if start_increment > datetime.date.today():
+                self.weekly = self.weekly.rename(
+                    columns={"Week": "date", "debt: (United States)": "relative_frequency"})
+                return
 
-        os.remove(
-            f"data/{keyword}/{str(start_date)}_{str(end_date)}/unadjusted/daily/temp.csv")
+    def get_monthly(self):
+        print("Downloading monthly data...")
+        token = self.get_token(f"{self.start_date} {self.end_date}")
 
-        start_increment += relativedelta(months=+6)
-        end_increment += relativedelta(months=+6)
+        url = f"https://trends.google.com/trends/api/widgetdata/multiline/csv?req=%7B%22time%22%3A%22{str(self.start_date)}%20{str(self.end_date)}%22%2C%22resolution%22%3A%22MONTH%22%2C%22locale%22%3A%22en-US%22%2C%22comparisonItem%22%3A%5B%7B%22geo%22%3A%7B%22country%22%3A%22US%22%7D%2C%22complexKeywordsRestriction%22%3A%7B%22keyword%22%3A%5B%7B%22type%22%3A%22BROAD%22%2C%22value%22%3A%22{self.keyword}%22%7D%5D%7D%7D%5D%2C%22requestOptions%22%3A%7B%22property%22%3A%22%22%2C%22backend%22%3A%22IZG%22%2C%22category%22%3A0%7D%7D&token={token}&tz=-120"
 
-        if start_increment > datetime.date.today():
-            break
+        self.monthly = pd.read_csv(url, header=1)
+        self.monthly = self.monthly.rename(
+            columns={"Month": "date", "debt: (United States)": "relative_frequency"})
 
-        index += 1
+    def get_token(self, timespan):
+        """
+        Retrieves a token from Google Trends, based on the keyword and timespan.
 
+        This function is a deritative of a function within Pytrends by github.com/GeneralMills.
+        Licensed under the Apache license, version 2.0.
+        Changes made by github.com/cristianpjensen to fit Njord's use case.
+        """
+        session = requests.session()
 
-def get_weekly(keyword, start_date, end_date):
-    print("Downloading weekly data...")
+        response = session.get(
+            url="https://trends.google.com/trends/api/explore",
+            timeout=(2, 5),
+            cookies=dict(filter(lambda i: i[0] == "NID", requests.get(
+                "https://trends.google.com/?geo=US",
+                timeout=(2, 5)
+            ).cookies.items())),
+            params={"hl": "en-US", "tz": -120,
+                    "req": '{"comparisonItem": [{"keyword": ' + f'"{self.keyword}"' + ', "time": ' + f'"{timespan}"' + ', "geo": "US"}], "category": 0, "property": ""}'}
+        )
 
-    start_increment = start_date
-    end_increment = start_date + relativedelta(years=+5)
-    index = 0
-
-    while True:
-        token = get_token(keyword, f"{start_increment} {end_increment}")
-
-        url = f"https://trends.google.com/trends/api/widgetdata/multiline/csv?req=%7B%22time%22%3A%22{str(start_increment)}%20{str(end_increment)}%22%2C%22resolution%22%3A%22WEEK%22%2C%22locale%22%3A%22en-US%22%2C%22comparisonItem%22%3A%5B%7B%22geo%22%3A%7B%22country%22%3A%22US%22%7D%2C%22complexKeywordsRestriction%22%3A%7B%22keyword%22%3A%5B%7B%22type%22%3A%22BROAD%22%2C%22value%22%3A%22{keyword}%22%7D%5D%7D%7D%5D%2C%22requestOptions%22%3A%7B%22property%22%3A%22%22%2C%22backend%22%3A%22IZG%22%2C%22category%22%3A0%7D%7D&token={token}&tz=-120"
-        urllib.request.urlretrieve(
-            url, f"data/{keyword}/{str(start_date)}_{str(end_date)}/unadjusted/weekly/temp.csv")
-
-        # Remove first two lines.
-        with open(f"data/{keyword}/{str(start_date)}_{str(end_date)}/unadjusted/weekly/temp.csv", "r") as f:
-            with open(f"data/{keyword}/{str(start_date)}_{str(end_date)}/unadjusted/weekly/{index}_weekly_{keyword}.csv", "w") as f1:
-                for _ in range(3):
-                    next(f)
-                f1.write(HEADER)
-                for line in f:
-                    f1.write(line)
-
-        os.remove(
-            f"data/{keyword}/{str(start_date)}_{str(end_date)}/unadjusted/weekly/temp.csv")
-
-        start_increment += relativedelta(years=+5)
-        end_increment += relativedelta(years=+5)
-
-        if start_increment > datetime.date.today():
-            break
-
-        index += 1
-
-
-def get_monthly(keyword, start_date, end_date):
-    print("Downloading monthly data...")
-    token = get_token(keyword, f"{start_date} {end_date}")
-
-    url = f"https://trends.google.com/trends/api/widgetdata/multiline/csv?req=%7B%22time%22%3A%22{str(start_date)}%20{str(end_date)}%22%2C%22resolution%22%3A%22MONTH%22%2C%22locale%22%3A%22en-US%22%2C%22comparisonItem%22%3A%5B%7B%22geo%22%3A%7B%22country%22%3A%22US%22%7D%2C%22complexKeywordsRestriction%22%3A%7B%22keyword%22%3A%5B%7B%22type%22%3A%22BROAD%22%2C%22value%22%3A%22{keyword}%22%7D%5D%7D%7D%5D%2C%22requestOptions%22%3A%7B%22property%22%3A%22%22%2C%22backend%22%3A%22IZG%22%2C%22category%22%3A0%7D%7D&token={token}&tz=-120"
-    urllib.request.urlretrieve(
-        url, f"data/{keyword}/{str(start_date)}_{str(end_date)}/unadjusted/temp.csv")
-
-    # Remove first two line
-    with open(f"data/{keyword}/{str(start_date)}_{str(end_date)}/unadjusted/temp.csv", "r") as f:
-        with open(f"data/{keyword}/{str(start_date)}_{str(end_date)}/unadjusted/monthly_{keyword}.csv", "w") as f1:
-            for _ in range(3):
-                next(f)
-            f1.write(HEADER)
-            for line in f:
-                f1.write(line)
-
-    os.remove(
-        f"data/{keyword}/{str(start_date)}_{str(end_date)}/unadjusted/temp.csv")
-
-
-def get_token(keyword, timespan):
-    """
-    Retrieves a token from Google Trends, based on the keyword and timespan.
-
-    This function is a deritative of a function within Pytrends by github.com/GeneralMills.
-    Licensed under the Apache license, version 2.0.
-    Changes made by github.com/cristianpjensen to fit Njord's use case.
-    """
-    session = requests.session()
-
-    response = session.get(
-        url="https://trends.google.com/trends/api/explore",
-        timeout=(2, 5),
-        cookies=dict(filter(lambda i: i[0] == "NID", requests.get(
-            "https://trends.google.com/?geo=US",
-            timeout=(2, 5)
-        ).cookies.items())),
-        params={"hl": "en-US", "tz": -120,
-                "req": '{"comparisonItem": [{"keyword": ' + f'"{keyword}"' + ', "time": ' + f'"{timespan}"' + ', "geo": "US"}], "category": 0, "property": ""}'}
-    )
-
-    content = response.text[4:]
-    return json.loads(content)["widgets"][0]["token"]
+        content = response.text[4:]
+        return json.loads(content)["widgets"][0]["token"]
 
 
 if __name__ == "__main__":
-    main()
+    Trends()
