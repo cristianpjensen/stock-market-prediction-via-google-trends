0b3e079209e3c041f3bf8dc4d4ce267a9222e720
Cristian Perez Jensen <cristianpjensen@gmail.com>
2020-07-22 16:40:22 +0200

    Improved CLI usage
    You can now specify which parameter to change and what to. However, documentation will have to be made for the different parameters and their respective options.
    
    Also, the YFinance output when downloading is now hidden.

--- scripts/feature_engineering.py
+++ scripts/feature_engineering.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import yfinance as yf
 import sys
+import getopt
+import os
 
 
 class FeatureEngineer():
@@ -14,7 +16,7 @@ class FeatureEngineer():
       - target (binary or bins).
     """
 
-    def __init__(self, INTERVAL="weekly", N=21, OFFSET=3, TYPE="delta", TARGET="binary"):
+    def __init__(self):
         """
         - `self.N` = amount of days to go in the past, for `Delta_N` and `Average_N`.
         - `self.INTERVAL` = daily or weekly data.
@@ -26,6 +28,21 @@ class FeatureEngineer():
         To use standard values, use `-` instead of values in the CLI.
         """
 
+        self.get_parameters()
+        self.import_data()
+        self.insert_target()
+        self.insert_type()
+        self.insert_lag()
+
+        if self.curated:
+            self.df.round(3).to_csv(
+                f"data/feature_engineered/{self.INTERVAL}-{self.TYPE}-{self.TARGET}-curated.csv", index=False)
+        else:
+            self.df.round(3).to_csv(
+                f"data/feature_engineered/{self.INTERVAL}-{self.TYPE}-{self.TARGET}.csv", index=False)
+
+    def get_parameters(self):
+        """Gets the parameters given by the user. If no parameter was given, then standard parameters will be used."""
         self.KW_LIST = ["debt", "color", "stocks", "restaurant", "portfolio", "inflation", "housing", "dow_jones", "revenue",
                         "economics", "credit", "markets", "return", "unemployment", "money", "religion", "cancer", "growth",
                         "investment", "hedge", "marriage", "bonds", "derivatives", "headlines", "profit", "society", "leverage",
@@ -36,40 +53,84 @@ class FeatureEngineer():
                         "arts", "culture", "bubble", "buy", "trader", "tourism", "politics", "energy", "consume", "consumption",
                         "freedom", "dividend", "world", "conflict", "kitchen", "forex", "home", "cash", "transaction", "garden",
                         "fond", "train", "labor", "fun", "environment", "ring"]
-        self.INTERVAL = INTERVAL
-        self.N = N
-        self.OFFSET = OFFSET
-        self.TYPE = TYPE
-        self.TARGET = TARGET
 
-        if len(sys.argv) != 5:
-            sys.exit(
-                "\nUsage: python3 feature_engineering.py INTERVAL N (days) TYPE TARGET (- to use default setting).\n")
+        argv = sys.argv[1:]
 
-        if sys.argv[1] != "-":
-            self.INTERVAL = sys.argv[1]
+        self.INTERVAL = "weekly"
+        self.N = 3
+        self.OFFSET = 0
+        self.TYPE = "pct_change"
+        self.TARGET = "binary"
+        self.curated = False
 
-        if sys.argv[2] != "-":
-            self.N = sys.argv[2]
+        try:
+            opts, args = getopt.getopt(argv, "i:n:o:t:r:c:",
+                                       ["interval=", "n=", "offset=", "type=",
+                                        "target=", "curation="])
+        except getopt.GetoptError:
+            print("Usage: feature_engineering.py -i <interval> -n <n> -o <offset> -t <feature type> -r <target type> -c <curation boolean>")
+            sys.exit(2)
 
-        if sys.argv[3] != "-":
-            self.TYPE = sys.argv[3]
+        for opt, arg in opts:
+            if opt in ["-i", "--interval"]:
+                if arg in ["daily", "weekly"]:
+                    self.INTERVAL = arg
+                else:
+                    print(
+                        "The interval has to be `daily` or `weekly`. Consult the documentation.")
+                    sys.exit(2)
 
-        if sys.argv[4] != "-":
-            self.TARGET = sys.argv[4]
+            if opt in ["-n", "--n"]:
+                try:
+                    arg = int(arg)
+                except ValueError:
+                    print(f"{arg} is not an integer.")
 
-        if self.INTERVAL == "weekly":
-            self.OFFSET = 0
-            self.N = int(self.N / 7)
+                if 0 < arg < 100:
+                    self.N = arg
+                else:
+                    print("0 < N < 100.")
+                    sys.exit(2)
 
-        self.import_data()
-        self.insert_target()
-        self.insert_type()
-        self.insert_lag()
-        self.insert_index()
+            if opt in ["-o", "--offset"]:
+                try:
+                    arg = int(arg)
+                except ValueError:
+                    print(f"{arg} is not an integer.")
 
-        self.df.round(3).to_csv(
-            f"data/feature_engineered/{self.INTERVAL}_{self.TYPE}_{self.TARGET}.csv", index=False)
+                if 0 <= arg < 100:
+                    self.OFFSET = arg
+                else:
+                    print("0 <= offset < 100.")
+                    sys.exit(2)
+
+            if opt in ["-t", "--type"]:
+                if arg in ["delta", "pct_change", "rolling"]:
+                    self.TYPE = arg
+                else:
+                    print(
+                        "The feature type has to be `delta`, `pct_change`, or `rolling`. Consult the documentation.")
+                    sys.exit(2)
+
+            if opt in ["-r", "--target"]:
+                if arg in ["binary", "bins"]:
+                    self.TARGET = arg
+                else:
+                    print(
+                        "The target has to be `binary` or `bins. Consult the documentation.")
+                    sys.exit(2)
+
+            if opt in ["-c", "--curated"]:
+                if arg == "True":
+                    self.curated = True
+                    self.KW_LIST = ["debt", "stocks", "dow_jones", "markets", "unemployment", "money", "stock_market",
+                                    "crisis", "nasdaq", "finance", "invest"]
+                elif arg == "False":
+                    self.curated = False
+                else:
+                    print(
+                        "Curated has to be `True` or `False`. Consult the documentation.")
+                    sys.exit(2)
 
     def import_data(self):
         """Imports data from Yahoo finance and the data files, containing Google Trends data."""
@@ -89,13 +150,15 @@ class FeatureEngineer():
                 f"data/{self.INTERVAL}/{kw}.csv")["Adjusted"]
 
         # Historical stock price data.
-        if self.INTERVAL == "daily":
-            self.ticker_df = yf.download("DJIA", period="max")
-        elif self.INTERVAL == "weekly":
-            self.ticker_df = yf.download("DJIA", period="max", interval="1wk")
-            self.kw_df["Date"] = pd.to_datetime(
-                self.kw_df["Date"], format="%Y-%m-%d")
-            self.kw_df["Date"] += pd.DateOffset(1)
+        with HiddenOutput():
+            if self.INTERVAL == "daily":
+                self.ticker_df = yf.download("DJIA", period="max")
+            elif self.INTERVAL == "weekly":
+                self.ticker_df = yf.download(
+                    "DJIA", period="max", interval="1wk")
+                self.kw_df["Date"] = pd.to_datetime(
+                    self.kw_df["Date"], format="%Y-%m-%d")
+                self.kw_df["Date"] += pd.DateOffset(1)
 
         # Manipulate stock price data.
         self.ticker_df["Change"] = self.ticker_df["Close"].pct_change()
@@ -130,20 +193,20 @@ class FeatureEngineer():
 
         if self.TYPE == "delta":
             for kw in self.KW_LIST:
-                i = self.N
+                i = self.N+1
                 while i < len(self.df):
-                    self.df.loc[i, f"{kw}"] = self.kw_df.loc[i,
-                                                             f"{kw}"] - self.kw_df.loc[i-self.N, f"{kw}"]
+                    self.df.loc[i, f"{kw}"] = self.kw_df.loc[i-1,
+                                                             f"{kw}"] - self.kw_df.loc[i-self.N-1, f"{kw}"]
                     i += 1
 
                 self.df[f"{kw}"] = self.df[f"{kw}"].shift(self.OFFSET)
 
-        elif self.TYPE == "dweek":
+        elif self.TYPE == "pct_change":
             for kw in self.KW_LIST:
-                i = self.N
-                while i < len(self.kw_df):
-                    self.df.loc[i, f"{kw}"] = self.kw_df.loc[i-7:i, f"{kw}"].mean(
-                    ) - self.kw_df.loc[i-self.N-7:i-self.N, f"{kw}"].mean()
+                i = self.N+1
+                while i < len(self.df):
+                    self.df.loc[i, f"{kw}"] = (
+                        self.kw_df.loc[i-1, f"{kw}"] - self.kw_df.loc[i-self.N-1, f"{kw}"]) / self.kw_df.loc[i-1, f"{kw}"]
                     i += 1
 
                 self.df[f"{kw}"] = self.df[f"{kw}"].shift(self.OFFSET)
@@ -159,20 +222,30 @@ class FeatureEngineer():
                 self.df[f"{kw}"] = self.df[f"{kw}"].shift(self.OFFSET)
 
         self.df = self.df[self.N+self.OFFSET:]
+        self.df = self.df.reset_index()
+
         self.df = self.ticker_df.merge(self.df, on="Date")
         self.df = self.df.drop("Date", axis=1)
+
+        self.df["index"] -= (self.N + 1)
 
     def insert_lag(self):
         """Adds a lag of 1 day feature."""
 
-        self.df.insert(1, "lag_1", self.df["Target"].shift(1))
-
+        self.df.insert(2, "lag_1", self.df["Target"].shift(1))
         self.df = self.df.drop(0)
 
-    def insert_index(self):
-        """Sets the index on the second column, because the Target has to be the first column for Google Cloud AI Platform."""
 
-        self.df.insert(1, "index", pd.Series(self.df.index).astype("int32"))
+class HiddenOutput():
+    """Hides the output of YFinance."""
+
+    def __enter__(self):
+        self._original_stdout = sys.stdout
+        sys.stdout = open(os.devnull, 'w')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout.close()
+        sys.stdout = self._original_stdout
 
 
 if __name__ == "__main__":
