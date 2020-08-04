892b6f06dcd2c3f5f09725df64348add7441450d
Cristian Perez Jensen <cristianpjensen@gmail.com>
2020-08-04 22:37:54 +0200

    src(features): rewrote build_features
    `build_features` was first built to be used for in the command line. But it is rewritten to be used
    in other scripts.

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,240 +1,259 @@
 import pandas as pd
 import yfinance as yf
-import sys
-import getopt
-import os
 
 
-class FeatureEngineer():
+def research(series, length=3):
     """
-    Feature engineers the Google Trends data, according to the specifications of the user.
+    The feature which is also used in the research paper by Tobias Preis et al.
 
-    Available features:
-      - interval (daily or weekly);
-      - amount of days to go into the past, used by rolling and delta;
-      - type (delta, dweek, or rolling);
-      - target (binary or bins).
+    Args:
+        series (pandas.Series): Series of the Google Trends data.
+        length (int, optional): Length of the moving average used in the
+            calculation. Defaults to 3.
+
+    Returns:
+        pandas.Series: Series containing the feature.
+
+    Raises:
+        ValueError: If length is less than 1 or greater than the length of the
+            series.
+        TypeError: If `series` is not of pandas.Series type or `length` is not an
+            integer.
+
     """
 
-    def __init__(self):
-        """
-        - `self.N` = amount of days to go in the past, for `Delta_N` and `Average_N`.
-        - `self.INTERVAL` = daily or weekly data.
-        - `self.OFFSET` = needed, because data is only available after three days.
-        - `self.KW_LIST` = a list of all search terms of which the data has been collected.
-        - `self.TYPE` = the type of feature the data is (delta, dweek, rolling).
-        - `self.TARGET` = the type of target used (binary or bins).
+    if not isinstance(length, int):
+        raise TypeError(f'`length` has to be of type int.')
 
-        To use standard values, use `-` instead of values in the CLI.
-        """
+    if not isinstance(series, pd.Series):
+        raise TypeError(f'`series` must be of type pandas.Series.')
 
-        self.get_parameters()
-        self.import_data()
-        self.insert_target()
-        self.insert_type()
-        self.insert_lag()
+    if length < 1 or length > series.size:
+        raise ValueError('`length` may not be less than 1 or greater than the \
+                          size of the series.')
 
-        if self.curated:
-            self.df.round(3).to_csv(
-                f"data/feature_engineered/{self.INTERVAL}-{self.TYPE}-{self.TARGET}-curated.csv", index=False)
-        else:
-            self.df.round(3).to_csv(
-                f"data/feature_engineered/{self.INTERVAL}-{self.TYPE}-{self.TARGET}.csv", index=False)
+    N = series.rolling(window=length).mean().shift(1)
+    delta_n = series - N
 
-    def get_parameters(self):
-        """Gets the parameters given by the user. If no parameter was given, then standard parameters will be used."""
-        self.KW_LIST = [line.rstrip("\n")
-                        for line in open("../data/keywords.txt")]
-
-        argv = sys.argv[1:]
-
-        self.INTERVAL = "weekly"
-        self.N = 3
-        self.OFFSET = 0
-        self.TYPE = "pct_change"
-        self.TARGET = "binary"
-        self.curated = False
-
-        try:
-            opts, args = getopt.getopt(argv, "i:n:o:t:r:c:",
-                                       ["interval=", "n=", "offset=", "type=",
-                                        "target=", "curation="])
-        except getopt.GetoptError:
-            print("Usage: feature_engineering.py -i <interval> -n <n> -o <offset> -t <feature type> -r <target type> -c <curation boolean>")
-            sys.exit(2)
-
-        for opt, arg in opts:
-            if opt in ["-i", "--interval"]:
-                if arg in ["daily", "weekly"]:
-                    self.INTERVAL = arg
-                else:
-                    print("--interval (-i) = daily | weekly")
-                    sys.exit(2)
-
-            if opt in ["-n", "--n"]:
-                try:
-                    arg = int(arg)
-                except ValueError:
-                    print(f"{arg} is not an integer.")
-
-                if 0 < arg < 100:
-                    self.N = arg
-                else:
-                    print("0 < N < 100")
-                    sys.exit(2)
-
-            if opt in ["-o", "--offset"]:
-                try:
-                    arg = int(arg)
-                except ValueError:
-                    print(f"{arg} is not an integer.")
-
-                if 0 <= arg < 100:
-                    self.OFFSET = arg
-                else:
-                    print("0 <= offset < 100.")
-                    sys.exit(2)
-
-            if opt in ["-t", "--type"]:
-                if arg in ["delta", "pct_change", "rolling"]:
-                    self.TYPE = arg
-                else:
-                    print("--type (-t) = delta | pct_change | rolling")
-                    sys.exit(2)
-
-            if opt in ["-r", "--target"]:
-                if arg in ["binary", "bins"]:
-                    self.TARGET = arg
-                else:
-                    print("--target (-r) = binary | bins")
-                    sys.exit(2)
-
-            if opt in ["-c", "--curated"]:
-                if arg == "True":
-                    self.curated = True
-                    self.KW_LIST = [line.rstrip("\n")
-                                    for line in open("curated.txt")]
-                elif arg == "False":
-                    self.curated = False
-                else:
-                    print("--curated (-c) = True | False")
-                    sys.exit(2)
-
-    def import_data(self):
-        """Imports data from Yahoo finance and the data files, containing Google Trends data."""
-
-        self.kw_df = pd.DataFrame()
-
-        # Google Trends data.
-        date = False
-        for kw in self.KW_LIST:
-
-            if not date:
-                self.kw_df["Date"] = pd.read_csv(
-                    f"data/{self.INTERVAL}/{kw}.csv")["Date"]
-                date = True
-
-            self.kw_df[f"{kw}"] = pd.read_csv(
-                f"data/{self.INTERVAL}/{kw}.csv")["Adjusted"]
-
-        # Historical stock price data.
-        with HiddenOutput():
-            if self.INTERVAL == "daily":
-                self.ticker_df = yf.download("DJIA", period="max")
-            elif self.INTERVAL == "weekly":
-                self.ticker_df = yf.download(
-                    "DJIA", period="max", interval="1wk")
-                self.kw_df["Date"] = pd.to_datetime(
-                    self.kw_df["Date"], format="%Y-%m-%d")
-                self.kw_df["Date"] += pd.DateOffset(1)
-
-        # Manipulate stock price data.
-        self.ticker_df["Change"] = self.ticker_df["Close"].pct_change()
-        self.ticker_df = self.ticker_df.drop(
-            ["Open", "High", "Low", "Adj Close", "Volume", "Close"], axis=1)
-        self.ticker_df = self.ticker_df.reset_index()
-
-        self.kw_df["Date"] = pd.to_datetime(
-            self.kw_df["Date"], format="%Y-%m-%d")
-
-    def insert_target(self):
-        """Inserts the type of target specified by the user."""
-
-        if self.TARGET == "binary":
-            self.ticker_df["Target"] = pd.cut(self.ticker_df["Change"],
-                                              bins=[-float("inf"),
-                                                    0, float("inf")],
-                                              labels=[0, 1])
-        elif self.TARGET == "bins":
-            self.ticker_df["Target"] = pd.cut(self.ticker_df["Change"],
-                                              bins=[-float("inf"), -0.025, -0.02, -0.015, -0.01, -0.005,
-                                                    0, 0.005, 0.01, 0.015, 0.02, 0.025, float("inf")],
-                                              labels=[-6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6])
-
-        self.ticker_df = self.ticker_df.drop("Change", axis=1)
-
-    def insert_type(self):
-        """Inserts the data features, specified by the user."""
-
-        self.df = pd.DataFrame()
-        self.df["Date"] = self.kw_df["Date"]
-
-        if self.TYPE == "delta":
-            for kw in self.KW_LIST:
-                i = self.N+1
-                while i < len(self.df):
-                    self.df.loc[i, f"{kw}"] = self.kw_df.loc[i-1,
-                                                             f"{kw}"] - self.kw_df.loc[i-self.N-1, f"{kw}"]
-                    i += 1
-
-                self.df[f"{kw}"] = self.df[f"{kw}"].shift(self.OFFSET)
-
-        elif self.TYPE == "pct_change":
-            for kw in self.KW_LIST:
-                i = self.N+1
-                while i < len(self.df):
-                    self.df.loc[i, f"{kw}"] = (
-                        self.kw_df.loc[i-1, f"{kw}"] - self.kw_df.loc[i-self.N-1, f"{kw}"]) / self.kw_df.loc[i-1, f"{kw}"]
-                    i += 1
-
-                self.df[f"{kw}"] = self.df[f"{kw}"].shift(self.OFFSET)
-
-        elif self.TYPE == "rolling":
-            for kw in self.KW_LIST:
-                i = self. N
-                while i < len(self.kw_df):
-                    self.df[f"{kw}"] = self.kw_df[f"{kw}"].rolling(
-                        self.N).mean()
-                    i += 1
-
-                self.df[f"{kw}"] = self.df[f"{kw}"].shift(self.OFFSET)
-
-        self.df = self.df[self.N+self.OFFSET:]
-        self.df = self.df.reset_index()
-
-        self.df = self.ticker_df.merge(self.df, on="Date")
-        self.df = self.df.drop("Date", axis=1)
-
-        self.df["index"] -= (self.N + 1)
-
-    def insert_lag(self):
-        """Adds a lag of 1 day feature."""
-
-        self.df.insert(2, "lag_1", self.df["Target"].shift(1))
-        self.df = self.df.drop(0)
+    return delta_n
 
 
-class HiddenOutput():
-    """Hides the output of YFinance."""
+def delta(series, length=3):
+    """
+    Feature based on the delta between two values `length` away.
 
-    def __enter__(self):
-        self._original_stdout = sys.stdout
-        sys.stdout = open(os.devnull, 'w')
+    Args:
+        series (pandas.Series): Series of Google Trends data.
+        length (int, optional): Difference between the two values of which the
+            delta is calculated. Defaults to 3.
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        sys.stdout.close()
-        sys.stdout = self._original_stdout
+    Returns:
+        pandas.Series: Series containing the feature.
+
+    Raises:
+        ValueError: If length is less than 1 or greater than the length of the
+            series.
+        TypeError: If `series` is not of pandas.Series type or `length` is not an
+            integer.
+
+    """
+
+    if not isinstance(length, int):
+        raise TypeError(f'`length` has to be of type int.')
+
+    if not isinstance(series, pd.Series):
+        raise TypeError(f'`series` must be of type pandas.Series.')
+
+    if length < 1 or length > series.size:
+        raise ValueError('`length` may not be less than 1 or greater than the \
+                          size of the series.')
+
+    return series.diff(periods=length)
 
 
-if __name__ == "__main__":
-    FeatureEngineer()
+def pct_change(series, length=3):
+    """
+    Feature based on the percentage change between two values `length` away.
+
+    Args:
+        series (pandas.Series): Series of Google Trends data.
+        length (int, optional): Percentage change between the two values of which
+            the delta is calculated. Defaults to 3.
+
+    Returns:
+        pandas.Series: Series containing the feature.
+
+    Raises:
+        ValueError: If length is less than 1 or greater than the length of the
+            series.
+        TypeError: If `series` is not of pandas.Series type or `length` is not an
+            integer.
+
+    """
+
+    if not isinstance(length, int):
+        raise TypeError(f'`length` has to be of type int.')
+
+    if not isinstance(series, pd.Series):
+        raise TypeError(f'`series` must be of type pandas.Series.')
+
+    if length < 1 or length > series.size:
+        raise ValueError('`length` may not be less than 1 or greater than the \
+                          size of the series.')
+
+    return series.pct_change(periods=length)
+
+
+def sma(series, length=6):
+    """
+    Simple moving average.
+
+    Args:
+        series (pandas.Series): Series of Google Trends data.
+        length (int, optional): Window of the moving average. Defaults to 6.
+
+    Returns:
+        pandas.Series: Series containing the feature.
+
+    Raises:
+        ValueError: If length is less than 1 or greater than the length of the
+            series.
+        TypeError: If `series` is not of pandas.Series type or `length` is not an
+            integer.
+
+    """
+
+    if not isinstance(length, int):
+        raise TypeError(f'`length` has to be of type int.')
+
+    if not isinstance(series, pd.Series):
+        raise TypeError(f'`series` must be of type pandas.Series.')
+
+    if length < 1 or length > series.size:
+        raise ValueError('`length` may not be less than 1 or greater than the \
+                          size of the series.')
+
+    return series.rolling(window=length)
+
+
+def ema(series, length=6):
+    """
+    Exponential moving average.
+
+    Args:
+        series (pandas.Series): Series of Google Trends data.
+        length (int, optional): Window of the moving average. Defaults to 6.
+
+    Returns:
+        pandas.Series: Series containing the feature.
+
+    Raises:
+        ValueError: If length is less than 1 or greater than the length of the
+            series.
+        TypeError: If `series` is not of pandas.Series type or `length` is not an
+            integer.
+
+    """
+
+    if not isinstance(length, int):
+        raise TypeError(f'`length` has to be of type int.')
+
+    if not isinstance(series, pd.Series):
+        raise TypeError(f'`series` must be of type pandas.Series.')
+
+    if length < 1 or length > series.size:
+        raise ValueError('`length` may not be less than 1 or greater than the \
+                          size of the series.')
+
+    return series.ewm(span=length, adjust=False)
+
+
+def lag(series, length=1):
+    """
+    Create `length` amount of lag features.
+
+    Args:
+        series (pandas.Series): Series of stock closing price.
+        length (int, optional): Amount of lag features. Defaults to 1.
+
+    Returns:
+        pandas.DataFrame: DataFrame containing all the lag features.
+
+    Raises:
+        ValueError: If length is less than 1 or greater than the length of the
+            series.
+        TypeError: If `series` is not of pandas.Series type or `length` is not an
+            integer.
+
+    """
+
+    if not isinstance(length, int):
+        raise TypeError(f'`length` has to be of type int.')
+
+    if not isinstance(series, pd.Series):
+        raise TypeError(f'`series` must be of type pandas.Series.')
+
+    if length < 1 or length > series.size:
+        raise ValueError('`length` may not be less than 1 or greater than the \
+                          size of the series.')
+
+    lag_df = pd.DataFrame()
+    for i in range(1, length):
+        lag_df[f"lag_{i}"] = series.shift(i)
+
+    return lag_df
+
+
+def target_binary(series):
+    """
+    Convert a series of stock prices to binary: up (1), down (0). This is used for
+    classifier algorithms.
+
+    Args:
+        series (pandas.Series): Series of stock closing price.
+
+    Returns:
+        pandas.Series: Series containing the stock closing price in binary.
+
+    Raises:
+        TypeError: If `series` is not of pandas.Series type.
+
+    """
+
+    if not isinstance(series, pd.Series):
+        raise TypeError('`series` must be of type pandas.Series.')
+
+    pct_change = series.pct_change()
+    binary_series = pd.cut(pct_change, bins=2, labels=False)
+
+    return binary_series
+
+
+def target_bins(series, bins=6):
+    """
+    Convert a series of stock prices to bins; used for classifier algorithms.
+
+    Args:
+        series (pandas.Series): Series of stock closing price.
+        bins (int, optional): Amount of bins used. Defaults to 6.
+
+    Returns:
+        pandas.Series: Series containing the stock closing price in specified
+            amount of bins.
+
+    Raises:
+        TypeError: If `series` is not of pandas.Series type or `bins` is not an
+            integer.
+
+    """
+
+    if not isinstance(bins, int):
+        raise TypeError('`bins` must be of type int.')
+
+    if not isinstance(series, pd.Series):
+        raise TypeError('`series` must be of type pandas.Series.')
+
+    pct_change = series.pct_change()
+    bins_series = pd.cut(pct_change, bins=bins, labels=False)
+
+    return bins_series
